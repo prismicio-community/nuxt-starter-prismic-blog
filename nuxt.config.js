fa5c8183b8926d4c60af025d930dabd48960bc2a
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-04-06 15:04:53 -1000

    feat: init refreshed project

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,71 +1,12 @@
 export default {
-  target: "static",
-
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: "Prismic + Nuxt Blog example",
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      {
-        hid: "description",
-        name: "description",
-        content: "Prismic + Nuxt Blog example"
-      }
-    ],
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-      {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css?family=Lato:300,400,700,900"
-      }
-    ]
-  },
-
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: "#fff" },
-
-  /*
-   ** Global CSS
-   */
-  css: ["@/assets/css/resetr.css", "@/assets/css/common.css"],
-
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [{ src: "~/plugins/prismicLinks", ssr: false }],
-
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-    // https://prismic-nuxt.js.org/
-    "@nuxtjs/prismic"
-  ],
-
-  prismic: {
-    endpoint: "https://your-repo-name.cdn.prismic.io/api/v2",
-    linkResolver: "@/plugins/link-resolver",
-    htmlSerializer: "@/plugins/html-serializer"
-  },
-
-  /*
-   ** Build configuration
-   */
+  buildModules: ["@nuxt/postcss8"],
   build: {
-    /*
-     ** You can extend webpack config here
-     */
-    extend(config, ctx) {
-      config.resolve.alias["vue"] = "vue/dist/vue.common";
-    }
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
   },
-
-  generate: {
-    fallback: "404.html" // Netlify reads a 404.html, Nuxt will load as an SPA
-  }
+  css: ["@/styles/global.css"],
 };
