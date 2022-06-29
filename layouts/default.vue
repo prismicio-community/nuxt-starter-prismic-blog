3a982d39ecd336ef8f96d53ee8c7d57e20dbf0de
Sam Littlefair <sam.littlefair@prismic.io>
2022-06-29 13:36:29 -0300

    docs: create documentation banner

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="text-slate-700">
+    <!-- TODO: Remove the following element once you have read the documentation. -->
+    <div
+      v-if="$config.development"
+      :style="{
+        background: '#5163ba',
+        padding: '1rem',
+        textAlign: 'center',
+        fontSize: '0.85rem',
+        color: '#fff'
+      }"
+    >
+      <p>
+        <strong>👋 Welcome to your new website!</strong> To customize the code
+        and content of this site,&nbsp;<a
+          href="https://github.com/prismicio-community/nuxt-starter-prismic-blog/tree/master/docs"
+          target="_blank"
+          rel="noreferrer"
+          :style="{
+            textDecoration: 'underline'
+          }"
+          >see the documentation</a
+        >. Remove this bar in <code>layouts/default.vue</code>.
+      </p>
+    </div>
     <Header
       v-if="navigation.data && settings.data"
       :with-profile="withHeaderProfile"
