6aaf03a2fb646f49d24c85f9ffe8f370b8c629a8
lihbr <lihbr@users.noreply.github.com>
2024-06-14 16:44:14 +0200

    fix: config update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ import { repositoryName } from './slicemachine.config.json'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4
+  },
+
   devtools: { enabled: true },
 
   app: {
